cf85ea78a54c5e30fc470aa54d5211f1a6968dde
rinonkia <gatapon1988@gmail.com>
2024-07-28 05:16:11 +0900

    Install godotenv and load config from .env file

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/rinonkia/cqs-repository-pattarn/handler"
 	"github.com/rinonkia/cqs-repository-pattarn/repository/command"
 	"github.com/rinonkia/cqs-repository-pattarn/repository/query"
@@ -14,8 +16,21 @@ import (
 	"gorm.io/gorm"
 )
 
+type envConfig struct {
+	host       string
+	port       string
+	dbUser     string
+	dbPassword string
+	dbName     string
+}
+
 func main() {
-	db, err := dbSetup()
+	c, err := loadEnv()
+	if err != nil {
+		log.Fatal(fmt.Errorf("load env error. %w", err))
+	}
+
+	db, err := dbSetup(c)
 	if err != nil {
 		log.Fatal(fmt.Errorf("db setup error. %w", err))
 	}
@@ -54,16 +69,11 @@ func main() {
 	log.Fatal(r.Run(":8080"))
 }
 
-func dbSetup() (*gorm.DB, error) {
-	var (
-		host   = "0.0.0.0"
-		port   = "3306"
-		user   = "crp"
-		pw     = "crp"
-		dbName = "crp"
+func dbSetup(c *envConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true",
+		c.dbUser, c.dbPassword, c.host, c.port, c.dbName,
 	)
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=true", user, pw, host, port, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		return nil, err
@@ -74,4 +84,43 @@ func dbSetup() (*gorm.DB, error) {
 	}
 
 	return db, nil
+}
+
+func loadEnv() (*envConfig, error) {
+	if err := godotenv.Load(); err != nil {
+		return nil, err
+	}
+
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		return nil, fmt.Errorf("DB_HOST cannot be an empty string")
+	}
+
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		return nil, fmt.Errorf("DB_PORT cannot be an empty string")
+	}
+
+	mysqlUser := os.Getenv("MYSQL_USER")
+	if mysqlUser == "" {
+		return nil, fmt.Errorf("MYSQL_USER cannot be an empty string")
+	}
+
+	mysqlUserPassword := os.Getenv("MYSQL_USER_PASSWORD")
+	if mysqlUserPassword == "" {
+		return nil, fmt.Errorf("MYSQL_USER_PASSWORD cannot be an empty string")
+	}
+
+	dbName := os.Getenv("DATABASE_NAME")
+	if dbName == "" {
+		return nil, fmt.Errorf("DATABASE_NAME cannot be an empty string")
+	}
+
+	return &envConfig{
+		host:       host,
+		port:       port,
+		dbUser:     mysqlUser,
+		dbPassword: mysqlUserPassword,
+		dbName:     dbName,
+	}, nil
 }
--- go.mod
+++ go.mod
@@ -2,7 +2,12 @@ module github.com/rinonkia/cqs-repository-pattarn
 
 go 1.21.0
 
-require github.com/gin-gonic/gin v1.10.0
+require (
+	github.com/gin-gonic/gin v1.10.0
+	github.com/joho/godotenv v1.5.1
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.25.11
+)
 
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
@@ -35,6 +40,4 @@ require (
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/mysql v1.5.7 // indirect
-	gorm.io/gorm v1.25.11 // indirect
 )
